cfd2384f7708ae1fac293bb4c8459d12b38b3794
Alex <alexwileyy@outlook.com>
2017-03-08 01:07:43 +0000

    Added extra security on authantication module

--- auth/authentication.js
+++ auth/authentication.js
@@ -1,6 +1,11 @@
 var db = require('../db/db.js');
 
 module.exports.verifySession = (clientID, cb) => {
+    if(!clientID){
+        return {
+            validated: false
+        };
+    }
     db.verifyID(clientID, function(user){
         if(user.session == clientID){
             var data = {
