68ce5c03a986eaaefcaf3e3c59e54c1c36accea5
Pandelis Zembashis <info@pandelis.me>
2017-03-10 18:52:50 +0000

    twilio texting middleware

---
+++ scripts/texting.js
@@ -0,0 +1,35 @@
+// Twilio Credentials
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const serviceSid = process.env.TWILIO_SERVICE_SID
+
+const client = require('twilio')(accountSid, authToken);
+
+
+function send(destinationNumber, msg){
+  client.sendMessage({
+    from: '+447481337641',
+    messagingServiceSid: serviceSid,
+    to: destinationNumber,
+    body: msg
+  }, function(err, data) {
+    if (err) {
+      console.error('Could not notify user ' + destinationNumber);
+      console.error(err);
+    } else {
+      console.log('User notified');
+    }
+  })
+}
+
+
+function sendMany(arrOfNumbers, msg){
+  arrOfNumbers.map( (x)=>send(x, msg) )
+}
+
+
+
+module.exports = {
+  send: send,
+  sendMany: sendMany
+}
